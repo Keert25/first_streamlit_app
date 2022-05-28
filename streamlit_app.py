58547bafa2a0dbc797e345a1d6b6cfe5d7431de6
Keert25 <105477527+Keert25@users.noreply.github.com>
2022-05-28 12:37:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from urllib.error import URLError
 
 streamlit.header('fruityvice fruit advice!')
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
- fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
